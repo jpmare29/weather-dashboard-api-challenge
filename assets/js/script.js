336653388199a1442b62ed9d7e66dea2f0b0b736
jpmare29 <jpmmare@yahoo.com>
2022-04-07 17:38:04 -0400

    typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ function formSubmitHandler(event) {
     let searchedState = stateInputEl.value;
     let searchCombo = searchedCity + ", " + searchedState;
     let newUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchedCity},${searchedState},US&limit=1&appid=2bd870c3ff12388ac354b0598f17ac0d`
-    //fetch geolocation api to tunr city and state into usable lat and long
+    //fetch geolocation api to turn city and state into usable lat and long
     fetch(newUrl).then(response => {
         return response.json();
     }).then(data => {
